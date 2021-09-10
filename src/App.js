e8a06c828c33c1de2a4e2ac083262b20456270b3
eFFx <chanoo104@naver.com>
2021-09-11 04:25:33 +0900

    Drawer 재작성, 상위 컴포턴트에 함수 전달 구현중

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React from "react";
 import { Schoolmeal } from "./schoolmeal";
-import { Bar } from "./bar";
+import Bar from "./bar";
+import drawer from "./drawer";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            drawerOpen: false
-        }
-    }
-    render() {
-        return (
-            <div>
-                <Bar data={"급식"} />
-                <Schoolmeal />
-            </div>
-        )
-    }
+    this.state = {
+      drawerOpen: false,
+    };
+  }
+  setDrawerOpen = (isDrawerOpen) => {
+    this.setState({
+      drawerOpen: isDrawerOpen,
+    });
+  };
+
+  toggleDrawer = () => {
+    this.setState((pstate) => ({ drawerOpen: !pstate.drawerOpen }));
+  };
+  render() {
+    return (
+      <div>
+        <Bar data={"급식"} click={console.log("asdf")} />
+        <Schoolmeal />
+        <drawer
+          setDrawerOpen={this.setDrawerOpen}
+          open={this.state.drawerOpen}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/bar.js
+++ src/bar.js
@@ -1,45 +1,59 @@
-import React from "react";
-import { AppBar, IconButton, makeStyles, Toolbar, Typography} from "@material-ui/core";
-import MenuIcon from '@material-ui/icons/Menu';
+import React, { Component } from "react";
+import {
+  AppBar,
+  IconButton,
+  makeStyles,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import { withStyles } from "@material-ui/styles";
 
-export const Bar = ({ data }) => {
-    const [state, setState] = React.useState({
-        left: false
-    });
-    const useStyles = makeStyles((theme) => ({
-        root: {
-            flexGrow: 1,
-        },
-        menuButton: {
-            marginRight: theme.spacing(2),
-        },
-        title: {
-            flexGrow: 1,
-            marginLeft: '-7px',
-            marginTop: '2px',
-            fontSize: '21px'
-        },
-        toolbar: {
-            minHeight: '55px',
-        }
-    }));
-    const classes = useStyles();
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: 2,
+  },
+  title: {
+    flexGrow: 1,
+    marginLeft: "-7px",
+    marginTop: "2px",
+    fontSize: "21px",
+  },
+  toolbar: {
+    minHeight: "55px",
+  },
+});
+
+class Bar extends Component {
+  render() {
+    const state = {
+      left: false,
+    };
+
+    const { classes } = this.props;
 
     return (
-
-        <div className={classes.root}>
-            <AppBar position="static" >
-                <Toolbar className={classes.toolbar}>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        {data}
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-        </div>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar className={classes.toolbar}>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+              onClick={this.props.click}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}></Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
     );
+  }
 }
 
-export default Bar;
+export default withStyles(styles)(Bar);
